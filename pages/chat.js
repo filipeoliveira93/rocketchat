f46a7a72652af211cf4da5be6c4b7c333da0cbfd
Filipe Oliveira <filipeoliveira0426@gmail.com>
2022-01-25 22:14:29 -0300

    dia 2

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+export default function PaginaDoCHat() {
+    return (
+        <div>
+            Pagina do chat
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,25 @@
 //componente react
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
-function GlobalStyle() {
-	return (
-		<style global jsx>{`
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-				list-style: none;
-			}
-			body {
-				font-family: 'Roboto', sans-serif; ;
-			}
-			/* App fit Height */
-			html,
-			body,
-			#__next {
-				min-height: 100vh;
-				display: flex;
-				flex: 1;
-			}
-			#__next {
-				flex: 1;
-			}
-			#__next > * {
-				flex: 1;
-			}
-			/* ./App fit Height */
-		`}</style>
-	);
-}
+
 
 function Title(props) {
 	console.log(props);
-	const Tag = props.tag || 'h1' // tag pode ser h1, h2, h3, p etc
+	const Tag = props.tag || "h1"; // tag pode ser h1, h2, h3, p etc
 	return (
 		<>
 			<Tag>{props.children}</Tag> {/*children é o conteudo do componente*/}
 			<style jsx>{`
 				${Tag} {
 					font-size: 1.5rem;
-					color: ${appConfig.theme.colors.neutrals['000']};
-		
-					
+					color: ${appConfig.theme.colors.neutrals["000"]};
 				}
 			`}</style>
 		</>
 	);
 }
-
 
 // function HomePage() {
 //     return (
@@ -73,14 +43,14 @@ function Title(props) {
 //   export default HomePage
 
 export default function PaginaInicial() {
-	const username = "filipeoliveira93";
-
+	
+	const [username, setUsername]= React.useState(''); //use state vai entregar um array com um valor e uma função para alterar o valor
+	const roteamento = useRouter();
 	return (
 		<>
-			<GlobalStyle />
+
 			<Box
 				styleSheet={{
-          
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "left",
@@ -103,17 +73,19 @@ export default function PaginaInicial() {
 						width: "100%",
 						maxWidth: "600px",
 						borderRadius: "10px",
-            height: "200px",
+						height: "200px",
 						padding: "32px",
 						margin: "60px",
-            marginTop: "-200px",
+						marginTop: "-200px",
 						boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
 						backgroundColor: appConfig.theme.colors.neutrals[900],
-            
 					}}>
 					{/* Formulário */}
 					<Box
 						as="form"
+						onSubmit= {(evento) => 
+							{evento.preventDefault();
+							roteamento.push('/chat');}} //muda para a página de chat quando o entrar é clickado
 						styleSheet={{
 							display: "flex",
 							flexDirection: "column",
@@ -123,29 +95,47 @@ export default function PaginaInicial() {
 							textAlign: "center",
 							marginBottom: "16px",
 						}}>
-						<Title tag="h1" >Boas vindas de volta!</Title>
+						<Title tag="h1">Boas vindas de volta!</Title>
 						<Text
 							variant="body3"
 							styleSheet={{
 								marginBottom: "20px",
 								color: appConfig.theme.colors.neutrals[400],
-
-                
 							}}>
 							{appConfig.name}
 						</Text>
-
+							{/* <input type="text"
+							 value={username}
+							 onChange={function(event) {
+								console.log(event.target.value);
+								//de onde vem o valor?
+								const valor = event.target.value;
+								//trocar o valor da variável e avisar quem precisa saber
+								setUsername(valor);
+								placeholder="Digite seu usuário GitHUb"
+							}}
+							></input> */}
 						<TextField
 							fullWidth
+							value={username}
+							placeHolder="Digite seu usuário GitHUb"
+							 onChange={function(event) {
+								console.log(event.target.value);
+								//de onde vem o valor?
+								const valor = event.target.value;
+								//trocar o valor da variável e avisar quem precisa saber
+								setUsername(valor);
+								
+							}}
 							textFieldColors={{
 								neutral: {
 									textColor: appConfig.theme.colors.neutrals[200],
 									mainColor: appConfig.theme.colors.neutrals[500],
 									mainColorHighlight: appConfig.theme.colors.primary[500],
 									backgroundColor: appConfig.theme.colors.neutrals[800],
-								},
-							}}
-						/>
+								}}}
+							></TextField>
+						
 						<Button
 							type="submit"
 							label="Entrar"
@@ -167,7 +157,7 @@ export default function PaginaInicial() {
 							flexDirection: "column",
 							alignItems: "center",
 							maxWidth: "180px",
-
+							minwidth: "180px",
 							padding: "0px",
 							backgroundColor: appConfig.theme.colors.neutrals[800],
 							border: "1px solid",
@@ -175,25 +165,24 @@ export default function PaginaInicial() {
 							borderRadius: "20px",
 							flex: 1,
 							Height: "250px",
-              overflow: "hidden",
+							overflow: "hidden",
 						}}>
 						<Image
 							styleSheet={{
-                
-                width: "90%",
-                marginTop: "15px",
+								width: "90%",
+								minwidth: "90%",
+								marginTop: "15px",
 								borderRadius: "50%",
 								marginBottom: "20px",
-                filter: "grayscale(100%)",
-               
+								filter: "grayscale(100%)",
 							}}
 							src={`https://github.com/${username}.png`}
 						/>
 						<Text
 							variant="body4"
 							styleSheet={{
-                fontSize: "14px",
-                weight: "100%",
+								fontSize: "14px",
+								weight: "100%",
 								color: appConfig.theme.colors.neutrals[200],
 								backgroundColor: appConfig.theme.colors.neutrals[900],
 								padding: "3px 60px",
